a6faf4dea40fb6335f07b73cb11a7dd04832503a
marc <marc.comas@gmail.com>
2019-01-09 17:00:06 +0100

    Nova versió de la web

--- colge.js
+++ colge.js
@@ -38,7 +38,9 @@ translate_labels = function(){
     if (labs.hasOwnProperty(i)){
       for(var j in labs[i]){
         if (labs[i].hasOwnProperty(j) & j.substring(0, 2) == 'l_'){
-          $('#' + j).html(get_text(i, j));
+          //console.log(j);
+          new_text = $('#' + j).html().replace(j, get_text(i, j));
+          $('#' + j).html(new_text);
         }
       }
     }
@@ -46,38 +48,63 @@ translate_labels = function(){
 }
 
 function refresh_covariates(){
+  
   if( !$('#cv_history_no').prop('checked') & !$('#cv_history_yes').prop('checked') ){
-    $('#sex').hide();
-    $('#htn').hide();
-    $('#colldl').hide();
-    $('#ami').hide();
-    $('#progressive').hide();
-    $('#recent').hide();
-    $('#polivascular').hide();
+    console.log("hiding");
+    $('#div_id_sex').hide();
+    $('#div_id_htn').hide();
+    $('#div_id_colldl').hide();
+    $('#div_id_ami').hide();
+    $('#div_id_progressive').hide();
+    $('#div_id_recent').hide();
+    $('#div_id_polivascular').hide();
     return '-1';
   }
   if( $('#cv_history_no').prop('checked') ){
-    $('#sex').show();
-    $('#htn').show();
-    $('#colldl').show();
-    $('#ami').hide();
-    $('#progressive').hide();
-    $('#recent').hide();
-    $('#polivascular').hide();
+    $('#div_id_sex').show();
+    $('#div_id_htn').show();
+    $('#div_id_colldl').show();
+    $('#div_id_ami').hide();
+    $('#div_id_progressive').hide();
+    $('#div_id_recent').hide();
+    $('#div_id_polivascular').hide();
     return '0';
   }
   if( $('#cv_history_yes').prop('checked') ){
-    $('#sex').hide();
-    $('#htn').hide();
-    $('#colldl').hide();
-    $('#ami').show();
-    $('#progressive').show();
-    $('#recent').show();
-    $('#polivascular').show();
+    $('#div_id_sex').hide();
+    $('#div_id_htn').hide();
+    $('#div_id_colldl').hide();
+    $('#div_id_ami').show();
+    $('#div_id_progressive').show();
+    $('#div_id_recent').show();
+    $('#div_id_polivascular').show();
     return '1';
   }
   $('#sex').show();
   return '-1';
+}
+
+function calculate_risk_fixed(x_smoking, x_cv, x_colldl){
+  x_age = Number($('#age_value').val());
+  x_ami = Number($('#ami_yes').prop('checked'));
+  x_progressive = Number($('#progressive_yes').prop('checked'));
+  x_polivascular = Number($('#polivascular_yes').prop('checked'));
+  x_recent = Number($('#recent_yes').prop('checked'));
+  x_diabetes = Number($('#diabetes_yes').prop('checked'));
+  x_male = Number($('#sex_male').prop('checked'));
+  x_htn = Number($('#htn_yes').prop('checked'));
+ 
+
+  if(x_cv == 1){
+    y_yes = 0.012 * x_age + 0.335 * x_ami + 0.501 * x_progressive + 0.617 * x_recent + 0.375 * x_polivascular + 0.441 * x_diabetes + 0.445 * x_smoking;
+    risk = 100 * (1 - Math.pow(0.911628151, Math.exp(y_yes)));
+  }
+  if(x_cv == 0){
+    y_no = 0.046 * x_age + 0.525 * x_male + 0.576 * x_diabetes + 0.485 * x_htn + 0.657 * x_smoking + 0.0028 * x_colldl;
+    risk = 100 * (1 - Math.pow(0.999094171, Math.exp(y_no)));
+  }
+
+  return risk;
 }
 
 function calculate_risk(){
@@ -130,25 +157,17 @@ function uncheck_form(){
   $('#polivascular_no').prop('checked', false);
   $('#polivascular_yes').prop('checked', false);
 
-  $('#advice').dialog("close");
-  $('#cv_risk_three_months').dialog("close");
-
   $('#result').html('');
+  $('#panel-result').hide();
   refreshUI();
 }
 
 function refreshUI(){
   var state = [];
   state['covariates'] = refresh_covariates();
-/*
-  console.log("State:", state);
-  if( state['smoking'] != '-1' & state['diet'] != '-1' & state['activity'] != '-1' & state['depression'] != '-1' & state['cv_risk'] != '-1'){
-    var intervention = health_intervention(state);
-    console.log("Intervention:", intervention);
-    build_advice(intervention, state)
-  }else{
-    build_advice_error(state);
-  }*/
+}
+round = function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
 
 function iniUI(){
@@ -160,9 +179,45 @@ function iniUI(){
   translate_labels();
 
   $('#l_calculate_risk').click(function() {
+    $('#panel-result').show();
     console.log("calculate risk");
-    risk = Math.round(calculate_risk());
-    $('#result').html('CV risk at 5 years: ' + risk + '%');
+    current_risk = round(calculate_risk(), 1);
+    message = 'CV risk at 5 years: <b>' + current_risk + '%</b>.<br />';
+    if(Number($('#smoking_yes').prop('checked')) == 1){
+       
+    }
+    x_smoking = Number($('#smoking_yes').prop('checked'));
+    x_cv = Number($('#cv_history_yes').prop('checked'));
+    x_colldl = Number($('#colldl_value').val());
+    ldl_high = false;
+    message = message + "<br/><ul>";
+    if(x_cv == 0 & x_colldl > 100){
+       ldl_high = true;
+       x_colldl = 100;
+       new_risk = round(calculate_risk_fixed(x_smoking, x_cv, x_colldl), 1);
+       console.log(current_risk);
+       console.log(new_risk);
+       if( current_risk - new_risk > 0 ){
+          current_risk = new_risk;
+          message = message + "<li><b>" + current_risk + "%</b> when LDL cholesterol at 100mg/dL.</li>";  
+       }
+    }
+    if(x_smoking == 1){
+       x_smoking = 0;
+       new_risk = round(calculate_risk_fixed(x_smoking, x_cv, x_colldl), 1);
+       console.log(current_risk);
+       console.log(new_risk);
+       if( current_risk - new_risk > 0 ){
+          current_risk = new_risk;
+          if(ldl_high){
+             message = message + "<li>Additionally, <b>" + current_risk + "%</b> for a non-smoker person.</li>";
+          }else{
+             message = message + "<li><b>" + current_risk + "%</b> for a non-smoker person.</li>";
+          }
+       }
+    }
+    message = message + "</ul>"; 
+    $('#result').html(message);
   });
 
   $('#l_clean_button').click(function() {
@@ -172,7 +227,11 @@ function iniUI(){
 
   $('#cv_history_yes').change(refreshUI);
   $('#cv_history_no').change(refreshUI);
+  $('#panel-result').hide();
   refreshUI();
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip({ boundary: 'window' })
+  })
 }
 
 $(document).ready(iniUI);
